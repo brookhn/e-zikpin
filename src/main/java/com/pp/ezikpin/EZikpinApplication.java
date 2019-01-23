338973a286df626bfa38911633448b892ff52f5f
yangtao <18111987@suningcultural.com>
2019-01-23 17:05:03 +0800

    dev

---
+++ src/main/java/com/pp/ezikpin/EZikpinApplication.java
@@ -0,0 +1,14 @@
+package com.pp.ezikpin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EZikpinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EZikpinApplication.class, args);
+    }
+
+}
+
